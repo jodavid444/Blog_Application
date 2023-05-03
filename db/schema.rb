b4bea4a4a093d89ff5fc460883745b98f5db070a
Joseph David <jodavid444dave@gmail.com>
2023-05-03 19:03:59 +0100

    Modify db folder

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_28_131854) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
-    t.bigint "post_id", null: false
+    t.integer "author_id", null: false
+    t.integer "post_id", null: false
     t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
@@ -27,8 +24,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_131854) do
   create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "post_id", null: false
-    t.bigint "author_id", null: false
+    t.integer "post_id", null: false
+    t.integer "author_id", null: false
     t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
   end
@@ -40,7 +37,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_131854) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "author_id", null: false
+    t.integer "author_id", null: false
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
