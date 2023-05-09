3f13c35b3f4c261316d85a53edcbf5d3c4d70a5e
Joseph David <jodavid444dave@gmail.com>
2023-05-09 14:28:25 +0100

    Create seeds for app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+user = User.create!(name: 'Tom', photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJHYna2rR4LtBNlA3gUSGxsmaiYY4PyxQEcA&usqp=CAU', bio: 'Student', posts_counter: 0)
+post = Post.create!(author_id: user.id , title: "Hello", text: 'Lorem ipsum', comments_counter: 0, likes_counter: 0)
+comment = Comment.create!(author_id: user.id, post_id: post.id, text: 'Good Job!')
+like = Like.create!(author_id: user.id, post_id: post.id)
