d0826598ce88d495971ab7188e93ddccb134a751
Joseph David <jodavid444dave@gmail.com>
2023-05-12 13:43:05 +0100

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy, foreign_key: 'author_id'
   has_many :posts, dependent: :destroy, foreign_key: 'author_id'
 
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
